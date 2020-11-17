d36951d8d0c28094e81043e43824089800d2fe8e
z3to <6gqrbj@posteo.org>
2020-11-17 17:36:06 +0100

    Region component

---
+++ components/Helper/Region.vue
@@ -0,0 +1,28 @@
+<template>
+  <span :class="`region region--${region}`" v-tooltip="{ content: tooltip }"><slot /></span>
+</template>
+
+<script>
+import { longScenario } from '~/assets/js/utils.js'
+
+export default {
+  props: ['region'],
+  computed: {
+    tooltip () {
+      return longScenario(this.region)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/global";
+
+  span.region {
+    text-decoration: underline;
+    text-decoration-style: dotted;
+    text-decoration-color: $color-neon;
+    text-decoration-thickness: 2px;
+  }
+
+</style>
