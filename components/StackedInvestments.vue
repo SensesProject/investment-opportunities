40a8bfe179d5441c7cfa6072391f78b7f45336b9
z3to <6gqrbj@posteo.org>
2020-11-18 13:16:20 +0100

    Fix Chrome + Safari

--- components/StackedInvestments.vue
+++ components/StackedInvestments.vue
@@ -118,14 +118,11 @@ export default {
     padding: 0;
     margin: 0;
     height: 70vh;
+    width: 100%;
 
     .vis {
       height: 100%;
       width: 100%;
-
-      figure, svg {
-
-      }
 
       svg > * {
         pointer-events: all;
@@ -135,10 +132,9 @@ export default {
     .vis-labels {
       pointer-events: none;
       position: absolute;
-      left: 0;
-      right: 0;
       top: 0;
-      bottom: 0;
+      height: 100%;
+      width: 100%;
       display: grid;
       grid-template-rows: repeat(4, 1fr);
     }
