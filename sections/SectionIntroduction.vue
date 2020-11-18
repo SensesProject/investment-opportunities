8143cbee05613f7e50b724db09d0b2dd305ceb43
z3to <6gqrbj@posteo.org>
2020-11-18 13:22:59 +0100

    Links and typo

--- sections/SectionIntroduction.vue
+++ sections/SectionIntroduction.vue
@@ -1,9 +1,13 @@
 <template>
   <section class="section wrapper">
-    <p>To achieve the Paris Agreement climate goals of limiting global warming to well below 2°C, pursuing efforts to limit it even to <a href="https://www.ipcc.ch/sr15/" target="_blank">1.5°C</a> requires bringing global CO<sub>2</sub> emissions to zero over the next 3–5 decades [Refer to IPCC 2018 SR1.5 and potentially other learn module]. This can be achieved through a complete transformation of global energy systems currently dominated by fossil fuels towards a future energy system characterized by high efficiency, broad electrification of end-uses and decarbonized power supply, mainly via renewables [refer to Luderer et al. 2018 and potentially other modules]. Given that energy system consist of large long-lived capital stocks, this transformation only comes about if there is an immediate shift in investment patterns that enables future energy systems to be low-carbon.</p>
-    <p>The Paris Agreement itself has recognized this crucial role of investment flows, by aiming, in its Article 2c), to: <cite>"Making finance flows consistent with a pathway towards low greenhouse gas emissions and climate-resilient development."</cite> This hasn’t gone unnoticed and its therefore not a surprise that various actors from the finance community are currently trying to understand the exact implications. The crucial questions are to understand what level of investments are required to be mobilized on a macro-level, to achieve the goals, but also to understand on a micro-level whether investment portfolios or investment plans of a company are aligned with the Paris Agreements’ long-term targets.</p>
+    <p>To achieve the Paris Agreement climate goals of limiting global warming to well below 2°C, pursuing efforts to limit it even to <a href="https://www.ipcc.ch/sr15/" target="_blank">1.5°C</a> requires bringing global CO<sub>2</sub> <a href="https://climatescenarios.org/emissions-gap/" target="_blank">emissions to zero</a> over the next 3–5 decades. This can be achieved through a complete <a v-tooltip="{ content: '<p>Luderer, G., Vrontisi, Z., Bertram, C. et al. “Residual fossil CO<sub>2</sub> emissions in 1.5–2 °C pathways.” Nature Clim Change 8 (2018): 626–633.</p>'}" href="https://doi.org/10.1038/s41558-018-0198-6" target="_blank">transformation of global energy systems</a> currently dominated by fossil fuels towards a future energy system characterized by high efficiency, broad electrification of end-uses and decarbonized power supply, mainly via renewables. Given that energy system consist of large long-lived capital stocks, this transformation only comes about if there is an immediate shift in investment patterns that enables future energy systems to be low-carbon.</p>
+    <p>The Paris Agreement itself has recognized this crucial role of investment flows, by aiming, in its Article 2c, to:</p>
+    <blockquote cite="https://unfccc.int/files/meetings/paris_nov_2015/application/pdf/paris_agreement_english_.pdf">“Making finance flows consistent with a pathway towards low greenhouse gas emissions and climate-resilient development.”</blockquote>
+    <p>This hasn’t gone unnoticed and its therefore not a surprise that various actors from the finance community are currently trying to understand the exact implications. The crucial questions are to understand what level of investments are required to be mobilized on a macro-level, to achieve the goals, but also to understand on a micro-level whether investment portfolios or investment plans of a company are aligned with the Paris Agreements’ long-term targets.</p>
     <p>In this context, this learn module wants to illustrate one relevant set of information, showing the size and composition of energy investments required for the next decade to achieve various levels of warming. It uses data from a recent multi-model scenario comparison.</p>
-    <p><em>By scrolling down, the module walks you through the key insights, and offers ways for further exploration along the way and at the end.</em></p>
+    <footer>
+      <p>By scrolling down, the module walks you through the key insights, and offers ways for further exploration along the way and at the end.</p>
+    </footer>
   </section>
 </template>
 
@@ -12,5 +16,14 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/global";
 
+  blockquote {
+    font-family: $font-sans;
+    font-style: italic;
+    font-size: 1.4rem;
+    line-height: 1.4;
+    margin: 2rem 0;
+    // letter-spacing: 0.01em;
+  }
 </style>
